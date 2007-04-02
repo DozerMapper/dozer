07f55476febbaf7f3b8718cb0a3bac8759c35adf
mhtierney <mhtierney@d5f72298-5529-0410-916d-ab124857a0e8>
2007-04-02 02:54:00 +0000

    repackage

--- src/test/java/net/sf/dozer/util/mapping/util/ApplicationBeanFactory.java
+++ src/test/java/net/sf/dozer/util/mapping/util/ApplicationBeanFactory.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.sf.dozer.util.mapping;
+package net.sf.dozer.util.mapping.util;
 
 import net.sf.dozer.util.mapping.util.MapperConstants;
 
