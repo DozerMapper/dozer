203774582316c779c5a15c68f36d2024972b7dde
mhtierney <mhtierney@d5f72298-5529-0410-916d-ab124857a0e8>
2007-05-05 03:58:16 +0000

    added empty constructor

--- src/test/java/net/sf/dozer/util/mapping/vo/allowedexceptions/TestException.java
+++ src/test/java/net/sf/dozer/util/mapping/vo/allowedexceptions/TestException.java
@@ -1,10 +1,10 @@
 package net.sf.dozer.util.mapping.vo.allowedexceptions;
 
 public class TestException extends RuntimeException {
+  public TestException() {
+  }
   
   public TestException(String msg) {
     super(msg);
   }
-  
-
 }
