e892615ddaec40b3dee7bba44073b583ba84ee03
mhtierney <mhtierney@d5f72298-5529-0410-916d-ab124857a0e8>
2007-05-10 02:36:01 +0000

    format

--- src/test/java/net/sf/dozer/util/mapping/KnownFailures.java
+++ src/test/java/net/sf/dozer/util/mapping/KnownFailures.java
@@ -15,7 +15,6 @@
  */
 package net.sf.dozer.util.mapping;
 
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,57 +29,55 @@ import net.sf.dozer.util.mapping.vo.map.NestedObjPrime;
 import net.sf.dozer.util.mapping.vo.map.SimpleObj;
 import net.sf.dozer.util.mapping.vo.map.SimpleObjPrime;
 
-
 /**
  * This is a holding grounds for test cases that reproduce known bugs, features, or gaps discovered during development.
- * As the use cases are resolved, these tests should be moved to the live unit test classes. 
+ * As the use cases are resolved, these tests should be moved to the live unit test classes.
  * 
  * @author tierney.matt
  */
 public class KnownFailures extends DozerTestBase {
-  
-  //Failure discovered during development of an unrelated map type feature request
-  
-  
-  //Failure discovered during development of an unrelated map type feature request  
+
+  // Failure discovered during development of an unrelated map type feature request
+
+  // Failure discovered during development of an unrelated map type feature request
   public void testMapType_NestedMapToVo_NoCustomMappings() throws Exception {
-    //Simple test checking that Maps get mapped to a VO without any custom mappings or map-id.
-    //Should behave like Vo --> Vo, matching on common attr(key) names.
+    // Simple test checking that Maps get mapped to a VO without any custom mappings or map-id.
+    // Should behave like Vo --> Vo, matching on common attr(key) names.
     Map nested2 = new HashMap();
     nested2.put("field1", "mapnestedfield1");
-    
+
     SimpleObj src = new SimpleObj();
     src.setNested2(nested2);
-    
+
     SimpleObjPrime result = (SimpleObjPrime) mapper.map(src, SimpleObjPrime.class);
     assertNotNull(result.getNested2());
     assertEquals(nested2.get("field1"), result.getNested2().getField1());
-    
+
     SimpleObj result2 = (SimpleObj) mapper.map(result, SimpleObj.class);
     assertEquals(src, result2);
   }
-  
+
   public void testMapType_MapToVo_CustomMapping_NoMapId() {
-    //Test nested Map --> Vo using custom mappings without map-id
-    mapper = getNewMapper(new String[]{"mapMapping3.xml"});
+    // Test nested Map --> Vo using custom mappings without map-id
+    mapper = getNewMapper(new String[] { "mapMapping3.xml" });
 
     NestedObj nested = new NestedObj();
     nested.setField1("field1Value");
-    
+
     Map nested2 = new HashMap();
     nested2.put("field1", "mapnestedfield1value");
     nested2.put("field2", "mapnestedfield2value");
-    
+
     SimpleObj src = new SimpleObj();
-    //src.setField1("field1value");
-    //src.setNested(nested);
+    // src.setField1("field1value");
+    // src.setNested(nested);
     src.setNested2(nested2);
-    
+
     SimpleObjPrime result = (SimpleObjPrime) mapper.map(src, SimpleObjPrime.class);
-    assertNull(result.getNested2().getField1());//field exclude in mappings file
+    assertNull(result.getNested2().getField1());// field exclude in mappings file
     assertEquals(nested2.get("field2"), result.getNested2().getField2());
   }
-  
+
   /*
    * Bug #1715496
    */
@@ -89,25 +86,22 @@ public class KnownFailures extends DozerTestBase {
     B b = new B();
     a.setList(new C[20000]);
     // Fill the list with C objects numbered from 0 to SIZE-1
-        for (int i = 0; i < a.getList().length; i++) {
-            a.getList()[i] = new C(i);
-        }
-
-        MapperIF mapper = DozerBeanMapperSingletonWrapper.getInstance();
-        mapper.map(a, b);
-        
-        // Check if C object nr i holds value i after the mapping 
-        for (int i = 0; i < b.getList().length; i++) {
-          if (b.getList()[i].getI() != i) {
-            int j = b.getList()[i].getI();
-        fail( "Object nr " + i + " owns same identityHashCode " + 
-            System.identityHashCode(b.getList()[i]) + " as Object nr " + j + 
-            ", " + System.identityHashCode(b.getList()[j]) + 
-            " => Object nr " + i + " is not mapped!");            
-          }
-        }
+    for (int i = 0; i < a.getList().length; i++) {
+      a.getList()[i] = new C(i);
     }
-  
 
+    MapperIF mapper = DozerBeanMapperSingletonWrapper.getInstance();
+    mapper.map(a, b);
+
+    // Check if C object nr i holds value i after the mapping
+    for (int i = 0; i < b.getList().length; i++) {
+      if (b.getList()[i].getI() != i) {
+        int j = b.getList()[i].getI();
+        fail("Object nr " + i + " owns same identityHashCode " + System.identityHashCode(b.getList()[i])
+            + " as Object nr " + j + ", " + System.identityHashCode(b.getList()[j]) + " => Object nr " + i
+            + " is not mapped!");
+      }
+    }
+  }
 
 }
