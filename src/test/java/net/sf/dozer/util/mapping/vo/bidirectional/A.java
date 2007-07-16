2492cd898f024e5f89976b81879f0b6b8f524d75
mhtierney <mhtierney@d5f72298-5529-0410-916d-ab124857a0e8>
2007-07-16 03:37:02 +0000

    merged 4.0_RELEASE tag to trunk

---
+++ src/test/java/net/sf/dozer/util/mapping/vo/bidirectional/A.java
@@ -0,0 +1,14 @@
+package net.sf.dozer.util.mapping.vo.bidirectional;
+
+public class A {
+  private B field1;
+
+  public B getField1() {
+    return field1;
+  }
+
+  public void setField1(B field1) {
+    this.field1 = field1;
+  }
+
+}
