7081118700965b6ac6764fc78e1c96a6fca2fc7a
buzdin <buzdin@d5f72298-5529-0410-916d-ab124857a0e8>
2009-06-30 20:14:56 +0000

    2813164 - Eclipse plugin is now properly published; Small test coverage fix;

---
+++ src/test/java/org/dozer/event/DozerEventTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2005-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.dozer.event;
+
+import junit.framework.TestCase;
+import org.dozer.classmap.ClassMap;
+import org.dozer.fieldmap.FieldMap;
+import static org.mockito.Mockito.mock;
+import org.junit.Test;
+
+/**
+ * @author dmitry.buzdin
+ */
+public class DozerEventTest extends TestCase {
+
+  private DozerEvent dozerEvent;
+
+  protected void setUp() throws Exception {
+    dozerEvent = new DozerEvent(DozerEventType.MAPPING_FINISHED,
+        mock(ClassMap.class),
+        mock(FieldMap.class),
+        mock(Object.class),
+        mock(Object.class),
+        mock(Object.class));
+  }
+
+  @Test
+  public void testToString() {
+    String result = dozerEvent.toString();
+    assertNotNull(result);
+  }
+  
+}
