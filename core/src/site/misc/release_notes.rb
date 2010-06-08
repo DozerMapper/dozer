f9232d9014e433bf175f550d1e93469ae91e0875
buzdin <buzdin@d5f72298-5529-0410-916d-ab124857a0e8>
2010-06-08 19:34:00 +0000

    release 5.2.2 preparations;

---
+++ core/src/site/misc/release_notes.rb
@@ -0,0 +1,41 @@
+#
+# Generates valid release notes section to be pasted into releasenotes.xml
+# Should be supplied with exact Url location from SF issue tracker
+#
+
+require 'open-uri'
+require 'rexml/document'
+
+include REXML
+
+#url = <<url
+#http://sourceforge.net/tracker/?limit=75&func=&group_id=133517&atid=727368&assignee=&status=1&category=&artgroup=&keyword=&submitter=&artifact_id=&assignee=&status=2&category=&artgroup=1129554&submitter=&keyword=&artifact_id=&submit=Filter&mass_category=&mass_priority=&mass_resolution=&mass_assignee=&mass_artgroup=&mass_status=&mass_cannedresponse=
+#url
+
+url = <<url
+http://sourceforge.net/tracker/?limit=75&func=&group_id=133517&atid=727371&assignee=&status=&category=&artgroup=&keyword=&submitter=&artifact_id=&assignee=&status=2&category=&artgroup=1129212&submitter=&keyword=&artifact_id=&submit=Filter
+url
+
+page = open(url, 'User-Agent' => 'Ruby-Wget').read
+
+tickets = []
+
+page.each_line {|l|
+
+  if l =~ /href.*func=detail.*?([0-9]+).*>(.*)<\/a>/
+   # puts l
+    id = $1
+    name = $2
+    l =~ /href="(.*)"/
+    url = $1.gsub(/\&/, "&amp;")
+    
+    tickets.push([id, name, url])
+  end
+
+}
+
+puts "<ul>"
+tickets.each {|i|
+  puts "<li>#{i[1]} <a href=\"#{i[2]}\">link</a></li>"
+}
+puts "</ul>"
---
+++ core/src/test/java/org/dozer/functional_tests/builder/DozerBuilderTest.java
@@ -0,0 +1,34 @@
+package org.dozer.functional_tests.builder;
+
+import org.junit.Test;
+
+public class DozerBuilderTest {
+
+  @Test
+  public void test() {
+
+//    DozerBeanMapper beanMapper = new DozerBeanMapper();
+//    DozerMappingBuilder mapping = new DozerMappingBuilder() {
+//
+//      protected void configure() {
+//        ContextMapping src = type(ContextMapping.class);
+//        ContextMappingPrime dest = type(ContextMappingPrime.class);
+//
+//        mapping(src, dest)
+//            .from(src.getLoanNo())
+//            .to(dest.getLoanNo());
+//      }
+//
+//      private <T> T type(Class<T> contextMappingClass) {
+//      }
+//
+//    };
+//
+//    mapping.apply(beanMapper);
+
+
+  }
+
+  private class DozerMappingBuilder {
+  }
+}
