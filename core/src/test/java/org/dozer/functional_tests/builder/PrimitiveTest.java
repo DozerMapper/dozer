555086c494985ccca27cb44a8e4fc19e4b74c2de
buzdin <buzdin@d5f72298-5529-0410-916d-ab124857a0e8>
2011-01-01 11:32:26 +0000

    feature; 2848458; BigDecimal <-> Double mapping;

--- core/src/test/java/org/dozer/functional_tests/builder/PrimitiveTest.java
+++ core/src/test/java/org/dozer/functional_tests/builder/PrimitiveTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.math.BigDecimal;
 import java.net.URL;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -26,18 +27,25 @@ public class PrimitiveTest extends Assert {
     source.file = "a";
     source.url = "http://a";
     source.type = "java.lang.String";
+    source.bigDecimal = new BigDecimal("1");
+    source.myDouble = new Double("1");
 
     Destination result = mapper.map(source, Destination.class);
 
     assertThat(result.file, equalTo(new File("a")));
     assertThat(result.url, equalTo(new URL("http://a")));
     assertThat(result.type, sameInstance(String.class));
+    assertThat(result.bigDecimal, equalTo(new Double("1")));
+    assertThat(result.myDouble, equalTo(new BigDecimal("1.0")));
   }
+
 
   public static class Source {
     String url;
     String type;
     String file;
+    BigDecimal bigDecimal;
+    Double myDouble;
 
     public String getUrl() {
       return url;
@@ -50,12 +58,22 @@ public class PrimitiveTest extends Assert {
     public String getFile() {
       return file;
     }
+
+    public BigDecimal getBigDecimal() {
+      return bigDecimal;
+    }
+
+    public Double getMyDouble() {
+      return myDouble;
+    }
   }
 
   public static class Destination {
     URL url;
     Class<String> type;
     File file;
+    Double bigDecimal;
+    BigDecimal myDouble;
 
     public void setUrl(URL url) {
       this.url = url;
@@ -67,6 +85,14 @@ public class PrimitiveTest extends Assert {
 
     public void setFile(File file) {
       this.file = file;
+    }
+
+    public void setBigDecimal(Double bigDecimal) {
+      this.bigDecimal = bigDecimal;
+    }
+
+    public void setMyDouble(BigDecimal myDouble) {
+      this.myDouble = myDouble;
     }
   }
 
