72c76531219686795d1f2ed7a1073dd0bead7fa5
Dmitry Spikhalskiy <dmitry@spikhalskiy.com>
2013-01-03 12:48:12 +0700

    Add failed test case for issue45

---
+++ core/src/test/java/org/dozer/vo/mapperaware/MapperAwareSimpleInternal.java
@@ -0,0 +1,8 @@
+package org.dozer.vo.mapperaware;
+
+/**
+ * @author Dmitry Spikhalskiy
+ * @since 03.01.13
+ */
+public class MapperAwareSimpleInternal {
+}
---
+++ core/src/test/java/org/dozer/vo/mapperaware/MapperAwareSimpleSrc.java
@@ -0,0 +1,17 @@
+package org.dozer.vo.mapperaware;
+
+/**
+ * @author Dmitry Spikhalskiy
+ * @since 03.01.13
+ */
+public class MapperAwareSimpleSrc {
+  private MapperAwareSimpleInternal one;
+
+  public MapperAwareSimpleInternal getOne() {
+    return one;
+  }
+
+  public void setOne(MapperAwareSimpleInternal one) {
+    this.one = one;
+  }
+}
