2e7339668165fc33e6564b5bd2a682255af10f34
Dmitry Spikhalskiy <dmitry@spikhalskiy.com>
2013-01-10 01:34:56 +0400

    Add simple test for failed auto polymorphic mapping

---
+++ core/src/test/java/org/dozer/functional_tests/InheritanceAutoPolyMappingTest.java
@@ -0,0 +1,36 @@
+package org.dozer.functional_tests;
+
+import org.dozer.Mapper;
+import org.dozer.vo.inheritance.autopoly.AContainer;
+import org.dozer.vo.inheritance.autopoly.C;
+import org.dozer.vo.inheritance.autopoly.XContainer;
+import org.dozer.vo.inheritance.autopoly.Z;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+/** Test suite for automatic polymorphic resolving and convertion
+ * @author Dmitry Spikhalskiy
+ * @since 10.01.13
+ */
+public class InheritanceAutoPolyMappingTest extends AbstractFunctionalTest {
+  private Mapper mapper;
+
+  @Override
+  @Before
+  public void setUp() throws Exception {
+    mapper = getMapper(new String[] {"autoPolyInheritanceMapping.xml"});
+  }
+
+  @Test
+  public void testCustomMappingForAbstractClasses() throws Exception {
+    AContainer aContainer = new AContainer();
+    aContainer.setField(new C());
+    XContainer result = mapper.map(aContainer, XContainer.class);
+
+    assertTrue(result.getField() instanceof Z);
+  }
+}
