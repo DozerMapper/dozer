d370bb6ded29303e6bc573929a3f1c295eb5b628
Pepe Barragan <pepebarragan@me.com>
2013-05-12 01:48:16 +0200

    Initial creation of new JAXBElement converter

---
+++ core/src/main/java/org/dozer/converters/JAXBElementConverter.java
@@ -0,0 +1,11 @@
+package org.dozer.converters;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author pepebarragan
+ *         Date: 5/10/13
+ *         Time: 2:52 PM
+ */
+public class JAXBElementConverter {
+}
