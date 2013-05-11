842b3b6ac0598695831ec82f5b937b44e445fa9b
Pepe Barragan <pepebarragan@me.com>
2013-05-12 01:53:11 +0200

    Support Calendar to String conversion

--- core/src/main/java/org/dozer/converters/CalendarConverter.java
+++ core/src/main/java/org/dozer/converters/CalendarConverter.java
@@ -15,65 +15,72 @@
  */
 package org.dozer.converters;
 
-import org.apache.commons.beanutils.Converter;
-
-import javax.xml.datatype.XMLGregorianCalendar;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.apache.commons.beanutils.Converter;
+
 /**
  * Internal class for converting Supported Data Types --> Calendar. Supported source data types include Date, Calendar,
  * String, Objects that return a long from their toString(). Only intended for internal use.
- * 
+ *
  * @author tierney.matt
  */
 public class CalendarConverter implements Converter {
 
-  private DateFormat dateFormat;
+	private DateFormat dateFormat;
 
-  public CalendarConverter(DateFormat dateFormat) {
-    this.dateFormat = dateFormat;
-  }
+	public CalendarConverter(DateFormat dateFormat) {
+		this.dateFormat = dateFormat;
+	}
 
-  public Object convert(Class destClass, Object srcObj) {
-    Calendar result = new GregorianCalendar();
-    Class srcFieldClass = srcObj.getClass();
-    // Convert from Date to Calendar
-    if (java.util.Date.class.isAssignableFrom(srcFieldClass)) {
-      result.setTime((java.util.Date) srcObj);
-    }
-    //  Convert from Calendar to Calendar
-    else if (Calendar.class.isAssignableFrom(srcFieldClass)) {
-      Calendar c = (Calendar) srcObj;
-      result.setTime(c.getTime());
-      result.setTimeZone(c.getTimeZone());
-    } else if (XMLGregorianCalendar.class.isAssignableFrom(srcFieldClass)) {
-      Calendar c = ((XMLGregorianCalendar) srcObj).toGregorianCalendar();
-      result.setTime(c.getTime());
-      result.setTimeZone(c.getTimeZone());
-    }
-    // String to Calendar
-    else if (dateFormat != null && String.class.isAssignableFrom(srcFieldClass)) {
-      try {
-        result.setTime(new Date(dateFormat.parse((String) srcObj).getTime()));
-      } catch (ParseException e) {
-        throw new ConversionException("Unable to parse source object using specified date format", e);
-      }
-      // Default conversion
-    } else {
-      try {
-        result.setTime(new Date(Long.parseLong(srcObj.toString())));
-      } catch (NumberFormatException e) {
-        throw new ConversionException("Unable to determine time in millis of source object", e);
-      }
-    }
-    return result;
-  }
+	public Object convert(Class destClass, Object srcObj) {
+		Calendar result = new GregorianCalendar();
+		Class srcFieldClass = srcObj.getClass();
+		// Convert from Date to Calendar
+		if (java.util.Date.class.isAssignableFrom(srcFieldClass)) {
+			result.setTime((java.util.Date) srcObj);
+		}
+		//  Convert from Calendar to Calendar
+		else if (Calendar.class.isAssignableFrom(srcFieldClass)) {
+			Calendar c = (Calendar) srcObj;
+			result.setTime(c.getTime());
+			result.setTimeZone(c.getTimeZone());
+		} else if (XMLGregorianCalendar.class.isAssignableFrom(srcFieldClass)) {
+			Calendar c = ((XMLGregorianCalendar) srcObj).toGregorianCalendar();
+			result.setTime(c.getTime());
+			result.setTimeZone(c.getTimeZone());
+		}
+		// String to Calendar
+		else if (dateFormat != null && String.class.isAssignableFrom(srcFieldClass)) {
+			try {
+				result.setTime(new Date(dateFormat.parse((String) srcObj).getTime()));
+			} catch (ParseException e) {
+				throw new ConversionException("Unable to parse source object using specified date format", e);
+			}
+			// Default conversion
+		} else {
+			try {
+				result.setTime(new Date(Long.parseLong(srcObj.toString())));
+			} catch (NumberFormatException e) {
+				throw new ConversionException("Unable to determine time in millis of source object", e);
+			}
+		}
 
-  public DateFormat getDateFormat() {
-    return dateFormat;
-  }
+		// Calendar to String
+		if (dateFormat != null && String.class.isAssignableFrom(destClass)) {
+			return dateFormat.format(result.getTime());
+		}
+
+		return result;
+	}
+
+	public DateFormat getDateFormat() {
+		return dateFormat;
+	}
 }
