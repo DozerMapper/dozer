309b3899367ccb3f9fc889c87885efbe75aefa7e
Dmitry Buzdin <buzdin@gmail.com>
2014-03-16 22:43:09 +0200

    Moving Activator to Core module; PAX Exam upgrade to 3.x;

--- osgi-test/src/test/java/org/dozer/osgi/OsgiContainerTest.java
+++ osgi-test/src/test/java/org/dozer/osgi/OsgiContainerTest.java
@@ -16,30 +16,27 @@
 package org.dozer.osgi;
 
 import org.dozer.DozerBeanMapper;
-import org.dozer.DozerInitializer;
-import org.dozer.util.DozerConstants;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.junit.Configuration;
-import org.ops4j.pax.exam.junit.ExamReactorStrategy;
-import org.ops4j.pax.exam.junit.JUnit4TestRunner;
-import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerMethod;
 import org.osgi.framework.BundleContext;
 
 import javax.inject.Inject;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
 import static org.ops4j.pax.exam.CoreOptions.*;
 import static org.ops4j.pax.exam.MavenUtils.asInProject;
 
 /**
  * @author Dmitry Buzdin
  */
-@RunWith(JUnit4TestRunner.class)
-@ExamReactorStrategy(AllConfinedStagedReactorFactory.class)
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerMethod.class)
 public class OsgiContainerTest {
 
   @Inject
