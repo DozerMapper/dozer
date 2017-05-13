c05cfa3403ffd2931ce03245cb3d973302bde17d
Gareth Healy <garethahealy@gmail.com>
2017-05-13 23:16:24 +0100

    Added checkstyle PackageDeclaration

--- core/src/misc/jmx-testengine/JMXTestEngine.java
+++ core/src/misc/jmx-testengine/JMXTestEngine.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.dozer.jmxtest;
 
 import java.util.ArrayList;
 import java.util.List;
