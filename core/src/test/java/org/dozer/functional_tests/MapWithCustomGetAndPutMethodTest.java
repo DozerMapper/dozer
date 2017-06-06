4167856babb74b4ceb1a3a89e2eb082571ee8028
Hannes Strydom <hannes.strydom@gmail.com>
2017-06-07 00:21:52 +1200

    Added functional tests

--- core/src/test/java/org/dozer/functional_tests/MapWithCustomGetAndPutMethodTest.java
+++ core/src/test/java/org/dozer/functional_tests/MapWithCustomGetAndPutMethodTest.java
@@ -15,94 +15,272 @@
  */
 package org.dozer.functional_tests;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-import org.dozer.vo.TestObject;
-import org.dozer.vo.map.MapToMap;
-import org.dozer.vo.map.MapToMapPrime;
-import org.junit.Before;
-import org.junit.Ignore;
+import org.dozer.DozerBeanMapper;
+import org.dozer.MappingException;
+import org.dozer.loader.api.BeanMappingBuilder;
+import org.dozer.loader.api.TypeDefinition;
+import org.hamcrest.number.IsCloseTo;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
-/**
- * @author Dmitry Buzdin
- */
 public class MapWithCustomGetAndPutMethodTest extends AbstractFunctionalTest {
 
-	/**
-	 * <mapping>
-		<class-a is-accessible="true">tsb.portal.controller.base.PortalResponse</class-a>
-		<class-b map-set-method="putUnknownType" map-get-method="get">tsb.grandcentral.collection.ObjectMap</class-b>
-		<!-- <field> -->
-		<!-- <a is-accessible="true">error</a> -->
-		<!-- <b key="error">this</b> -->
-		<!-- </field> -->
-		<field>
-			<a>errorCode</a>
-			<b key="errorCode">this</b>
-		</field>
-		<field>
-			<a>errorMessage</a>
-			<b key="errorMessage">this</b>
-		</field>
-		<field-exclude>
-			<a>error</a>
-			<b>error</b>
-		</field-exclude>
-		</mapping>
-	 */
 
-	
-	/**
-	 @Autowired
-	private DozerBeanMapper mapper;
+  @Test
+  public void testDefaultMapBehaviour_UseDefaultGetAndPutMethod() {
+    DozerBeanMapper defaultMapper = new DozerBeanMapper();
+    
+    // Map to Object, should use "get"
+    MapWithCustomGetAndPut input1 = MapWithCustomGetAndPut.createInput();
+    ValueContainer output1 = defaultMapper.map(input1, ValueContainer.class);
 
-	@Test
-	public void mapObjectMapWithErrorMessageTOPortalRespone_ValidSessionId_IdenticalOutput() {
-		String errorMessage = "messyage";
+    assertThat(output1.getEmptyString(), is(""));
+    assertThat(output1.getStringValue(), is("String"));
+    assertThat(output1.getIntValue(), is(123));
+    assertThat(output1.getDoubleValue(), is(IsCloseTo.closeTo(123.456, 0.00001)));
+    
+    
+    // Object to Map, should use "put"
+    ValueContainer input2 = ValueContainer.createInput();
+    MapWithCustomGetAndPut output2 = defaultMapper.map(input2, MapWithCustomGetAndPut.class);
 
-		ObjectMap input = new ObjectMap();
-		input.add("errorMessage", errorMessage);
+    assertThat(output2.get("emptyString"), is(""));
+    assertThat(output2.get("stringValue"), is("Different String"));
+    assertThat(output2.get("intValue"), is(-987));
+    assertThat(output2.get("doubleValue"), is(-987.654));
+  }
 
-		PortalResponse response = this.mapper.map(input, PortalResponse.class);
-		assertThat(response.getErrorMessage(), is(equalTo(errorMessage)));
-		assertThat(response.getError(), is(true));
-		assertThat(response.getErrorCode(), is(nullValue()));
-	}
+  @Test
+  public void testMapWithCustomMethods_UseSpecifiedMethods() {
+    DozerBeanMapper customMapper = new DozerBeanMapper();
+    customMapper.addMapping(new BeanMappingBuilder() {
+      @Override
+      protected void configure() {
+        mapping(
+            ValueContainer.class, 
+            new TypeDefinition(MapWithCustomGetAndPut.class).mapMethods("customGet", "customPut")
+            );
+      }
+    });
+    
+    // Map to Object, should use "customGet"
+    MapWithCustomGetAndPut input1 = MapWithCustomGetAndPut.createInput();
+    ValueContainer output1 = customMapper.map(input1, ValueContainer.class);
 
-	@Test
-	public void mapObjectMapWithErrorMessageAndCodeTOPortalRespone_ValidSessionId_IdenticalOutput() {
-		String errorMessage = "coded message";
-		String errorCode = "911";
+    assertThat(output1.getEmptyString(), is("Map contains EMPTY"));
+    assertThat(output1.getStringValue(), is("String"));
+    assertThat(output1.getIntValue(), is(123));
+    assertThat(output1.getDoubleValue(), is(IsCloseTo.closeTo(123.456, 0.00001)));
 
-		ObjectMap input = new ObjectMap();
-		input.add("errorMessage", errorMessage);
-		input.add("errorCode", errorCode);
+    // Object to Map, should use "customPut"
+    ValueContainer input = ValueContainer.createInput();
+    MapWithCustomGetAndPut output = customMapper.map(input, MapWithCustomGetAndPut.class);
 
-		PortalResponse response = this.mapper.map(input, PortalResponse.class);
-		assertThat(response.getErrorMessage(), is(equalTo(errorMessage)));
-		assertThat(response.getError(), is(true));
-		assertThat(response.getErrorCode(), is(equalTo(errorCode)));
-	}
+    assertThat(output.get("emptyString"), is("Tried to insert EMPTY"));
+    assertThat(output.get("stringValue"), is("Different String"));
+    assertThat(output.get("intValue"), is(987)); // only the Int should be absolute
+    assertThat(output.get("doubleValue"), is(-987.654));
+  }
 
-	@Test
-	public void mapObjectMapWithNoErrorMessageTOPortalRespone_ValidSessionId_IdenticalOutput() {
-		ObjectMap input = new ObjectMap();
+  @Test
+  public void testMapWithNullGetAndPutMethods_FallbackToDefaultMethods() {
+    DozerBeanMapper nullMapper = new DozerBeanMapper();
+    nullMapper.addMapping(new BeanMappingBuilder() {
+      @Override
+      protected void configure() {
+        mapping(
+            ValueContainer.class, 
+            new TypeDefinition(MapWithCustomGetAndPut.class).mapMethods(null, null)
+            );
+      }
+    });
+    
+    // Map to Object, should use "get"
+    MapWithCustomGetAndPut input1 = MapWithCustomGetAndPut.createInput();
+    ValueContainer output1 = nullMapper.map(input1, ValueContainer.class);
+    
+    assertThat(output1.getEmptyString(), is(""));
+    assertThat(output1.getStringValue(), is("String"));
+    assertThat(output1.getIntValue(), is(123));
+    assertThat(output1.getDoubleValue(), is(IsCloseTo.closeTo(123.456, 0.00001)));
+    
+    // Object to Map, should use "put"
+    ValueContainer input2 = ValueContainer.createInput();
+    MapWithCustomGetAndPut output2 = nullMapper.map(input2, MapWithCustomGetAndPut.class);
+    
+    assertThat(output2.get("emptyString"), is(""));
+    assertThat(output2.get("stringValue"), is("Different String"));
+    assertThat(output2.get("intValue"), is(-987));
+    assertThat(output2.get("doubleValue"), is(-987.654));
+  }
+  
+  @Test
+  public void testMapWithEmptyGetAndPutMethods_FallbackToDefaultMethods() {
+    DozerBeanMapper emptyMapper = new DozerBeanMapper();
+    emptyMapper.addMapping(new BeanMappingBuilder() {
+      @Override
+      protected void configure() {
+        mapping(
+            ValueContainer.class, 
+            new TypeDefinition(MapWithCustomGetAndPut.class).mapMethods("", "")
+            );
+      }
+    });
+    
+    // Map to Object, should use "get"
+    MapWithCustomGetAndPut input1 = MapWithCustomGetAndPut.createInput();
+    ValueContainer output1 = emptyMapper.map(input1, ValueContainer.class);
+    
+    assertThat(output1.getEmptyString(), is(""));
+    assertThat(output1.getStringValue(), is("String"));
+    assertThat(output1.getIntValue(), is(123));
+    assertThat(output1.getDoubleValue(), is(IsCloseTo.closeTo(123.456, 0.00001)));
+    
+    // Object to Map, should use "put"
+    ValueContainer input2 = ValueContainer.createInput();
+    MapWithCustomGetAndPut output2 = emptyMapper.map(input2, MapWithCustomGetAndPut.class);
+    
+    assertThat(output2.get("emptyString"), is(""));
+    assertThat(output2.get("stringValue"), is("Different String"));
+    assertThat(output2.get("intValue"), is(-987));
+    assertThat(output2.get("doubleValue"), is(-987.654));
+  }
+  
+  /**
+   * This is simply to make sure we are trying to be 'too intelligent' and still throwing exceptions
+   * if non-existent methods are configured.
+   */
+  @Test(expected=MappingException.class)
+  public void testMapWithInvalidGetMethod_ThrowsMappingException() {
+    DozerBeanMapper invalidMapper = new DozerBeanMapper();
+    invalidMapper.addMapping(new BeanMappingBuilder() {
+      @Override
+      protected void configure() {
+        mapping(
+            ValueContainer.class, 
+            new TypeDefinition(MapWithCustomGetAndPut.class).mapMethods("invalidGetMethod", "invalidPutMethod")
+            );
+      }
+    });
+    
+    // Map to Object, will try to  use "invalidGetMethod"
+    MapWithCustomGetAndPut input = MapWithCustomGetAndPut.createInput();
+    invalidMapper.map(input, ValueContainer.class);
+    
+    fail("Expected MappingException");
+  }
+  
+  /**
+   * This is simply to make sure we are trying to be 'too intelligent' and still throwing exceptions
+   * if non-existent methods are configured.
+   */
+  @Test(expected=MappingException.class)
+  public void testMapWithInvalidPutMethod_ThrowsMappingException() {
+    DozerBeanMapper invalidMapper = new DozerBeanMapper();
+    invalidMapper.addMapping(new BeanMappingBuilder() {
+      @Override
+      protected void configure() {
+        mapping(
+            ValueContainer.class, 
+            new TypeDefinition(MapWithCustomGetAndPut.class).mapMethods("invalidGetMethod", "invalidPutMethod")
+            );
+      }
+    });
 
-		PortalResponse response = this.mapper.map(input, PortalResponse.class);
-		assertThat(response.getError(), is(false));
-		assertThat(response.getErrorMessage(), is(nullValue()));
-		assertThat(response.getErrorCode(), is(nullValue()));
-	}
-	 */
+    // Object to Map, will try to  use "invalidPutMethod"
+    ValueContainer input = ValueContainer.createInput();
+    invalidMapper.map(input, MapWithCustomGetAndPut.class);
+
+    fail("Expected MappingException");
+  }
+
+  protected static class MapWithCustomGetAndPut extends HashMap<String, Object> {
+
+    private static final long serialVersionUID = 3085080961637343951L;
+
+    public Object customGet(Object key) {
+      Object value = super.get(key);
+
+      if (value.equals("")) {
+        return "Map contains EMPTY";
+      } else {
+        return value;
+      }
+    }
+
+    public Object customPut(String key, Object value) {
+      Object valueToInsert = value;
+
+      if (value.equals("") ) {
+        valueToInsert = "Tried to insert EMPTY";
+      } else if (value instanceof Integer) {
+        // for testing purposes, we convert integers to absolute value
+        valueToInsert = Math.abs((Integer)value);
+      }
+
+      super.put(key, valueToInsert);
+
+      return valueToInsert;
+    }
+
+    public static MapWithCustomGetAndPut createInput() {
+      MapWithCustomGetAndPut result = new MapWithCustomGetAndPut();
+      result.put("emptyString", "");
+      result.put("stringValue", "String");
+      result.put("intValue", 123);
+      result.put("doubleValue", 123.456);
+      return result;
+    }
+  }
+
+  protected static class ValueContainer {
+
+    private String emptyString;
+    private String stringValue;
+    private int intValue;
+    private double doubleValue;
+
+    public String getEmptyString() {
+      return emptyString;
+    }
+    public void setEmptyString(String nullString) {
+      this.emptyString = nullString;
+    }
+
+    public String getStringValue() {
+      return stringValue;
+    }
+    public void setStringValue(String stringValue) {
+      this.stringValue = stringValue;
+    }
+
+    public int getIntValue() {
+      return intValue;
+    }
+    public void setIntValue(int intValue) {
+      this.intValue = intValue;
+    }
+
+    public double getDoubleValue() {
+      return doubleValue;
+    }
+    public void setDoubleValue(double doubleValue) {
+      this.doubleValue = doubleValue;
+    }
+
+    public static ValueContainer createInput() {
+      ValueContainer result = new ValueContainer();
+      result.emptyString = "";
+      result.stringValue = "Different String";
+      result.intValue = -987;
+      result.doubleValue = -987.654;
+      return result;
+    }
+  }
+
 }
