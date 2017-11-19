d7ecf30311a9711fa1324132448008d725e5342b
Vadim <0xvadeg@gmail.com>
2017-11-19 20:58:13 +0100

    PR #382: Added spring-boot autoconfiguration support. (#526)
    * Added spring-boot autoconfiguration support.
    
    Fixes PR#382
    - Spring boot autoconfiguration module added.
    - Spring boot starter module added.
    - Added some integration tests for checking Dozer autoconfiguration.
    - Test logging set to "WARN" when tests runs using 'maven-surefire-plugin'. It increases logs readability.
    
    * Some review comments fixes.
    
    * Fix review comments.
    
    * Removed useless ApplicationContext dependency from DozerAutoConfiguration.
    
    * Move "spring-boot-dependencies" into dozer "bom-dependencies".
    Rename modules.

---
+++ dozer-spring-boot-auto-configuration/dozer-spring-boot-autoconfigure/src/main/java/com/github/dozermapper/springboot/autoconfigure/DozerConfigurationProperties.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2005-2017 Dozer Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.dozermapper.springboot.autoconfigure;
+
+import java.util.Arrays;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.io.Resource;
+
+
+
+/**
+ * Dozer configuration properties.
+ */
+@ConfigurationProperties(prefix = "dozer")
+public class DozerConfigurationProperties {
+
+    /**
+     * Mapping files configuration.
+     * For example <code>classpath:*.dozer.xml</code>.
+     */
+    private Resource[] mappingFiles = new Resource[] {};
+
+    /**
+     * Mapping files configuration.
+     *
+     * @return mapping files
+     */
+    public Resource[] getMappingFiles() {
+        return Arrays.copyOf(mappingFiles, mappingFiles.length);
+    }
+
+    /**
+     * Set mapping files configuration. For example <code>classpath:*.dozer.xml</code>.
+     *
+     * @param mappingFiles dozer mapping files
+     * @return dozer properties
+     */
+    public DozerConfigurationProperties setMappingFiles(Resource[] mappingFiles) {
+        this.mappingFiles = Arrays.copyOf(mappingFiles, mappingFiles.length);
+        return this;
+    }
+}
