601f7370d5ec01eb4788141071d62c8f4dccb3d7
Gareth Healy <garethahealy@gmail.com>
2018-04-01 16:59:13 +0100

    Updated to next snapshot

--- docs/release.sh
+++ docs/release.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-rm -rf ././../../DozerMapper-github-io/gitbook/
-rm -rf ././../../DozerMapper-github-io/dtd/
-rm -rf ././../../DozerMapper-github-io/schema/
+echo "Removing old files in DozerMapper-github-io"
+rm -rf .././../../DozerMapper-github-io/gitbook/
+rm -rf .././../../DozerMapper-github-io/dtd/
+rm -rf .././../../DozerMapper-github-io/schema/
 
+echo "Building..."
 ./build-gitbook.sh
 
-mv gitbook ././../../DozerMapper-github-io/gitbook/
-mv user-guide.pdf ././../../DozerMapper-github-io/user-guide.pdf
+echo "Copying to DozerMapper-github-io repo..."
+mv gitbook .././../../DozerMapper-github-io/gitbook/
+mv user-guide.pdf .././../../DozerMapper-github-io/user-guide.pdf
 
-cp -R ././../schema/src/main/resources/dtd/ ././../../DozerMapper-github-io/dtd
-cp -R ././../schema/src/main/resources/schema/ ././../../DozerMapper-github-io/schema
+cp -R ././../core/target/classes/dtd/ .././../../DozerMapper-github-io/dtd
+cp -R ././../core/target/classes/schema/ .././../../DozerMapper-github-io/schema
