aa2a575abe926f5d46d0555eedcc5db32e6376f1
garethahealy <garethahealy@gmail.com>
2019-06-17 09:52:58 +0100

    Updated to next snapshot

--- core/src/main/java/com/github/dozermapper/core/util/DozerConstants.java
+++ core/src/main/java/com/github/dozermapper/core/util/DozerConstants.java
@@ -28,7 +28,7 @@ public final class DozerConstants {
     private DozerConstants() {
     }
 
-    public static final String CURRENT_VERSION = "6.5.0";
+    public static final String CURRENT_VERSION = "6.6.0-SNAPSHOT";
 
     public static final boolean DEFAULT_WILDCARD_POLICY = true;
     public static final boolean DEFAULT_WILDCARD_CASE_INSENSITIVE_POLICY = false;
